c88cbe7f476f1af5e87b29147def82b3e2f26fed
ux_lsl <ux_lsl@163.com>
2015-07-23 17:27:05 +0800

    增加初始化 sdcv

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,6 +2,7 @@
 (require 'epy-setup)
 (require 'epy-python)
 (require 'epy-completion)
+(require 'sdcv)
 (global-set-key (kbd "<f7>") 'flycheck-list-errors)
 (global-set-key (kbd "<f8>") 'flycheck-previous-error)
 (global-set-key (kbd "<f9>") 'flycheck-next-error)
